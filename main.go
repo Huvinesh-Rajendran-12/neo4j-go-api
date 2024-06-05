060fa9ec5d7db0b007cae1709b20e2e84c1098f8
Huvinesh Rajendran <huvinesh@teleme.co>
2024-06-05 17:10:14 +0800

    added trusted proxies
    set trusted proxies in gin framework

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 		panic("Error loading .env file")
 	}
 	r := gin.Default()
+    r.SetTrustedProxies([]string{"47.254.238.67", "127.0.0.1"})
     api := r.Group("/api")
     api.POST("/authenticate", handlers.AdminAuthentication)
     api.POST("/generate/token", handlers.CreateAPIToken)
